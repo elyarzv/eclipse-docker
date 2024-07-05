9128b51b12b07275922df415edd9c5451a046961
Elyar <elyar.zavary@gmail.com>
2024-07-05 13:13:17 -0700

    Update run_docker.ps1

--- windows/run_docker.ps1
+++ windows/run_docker.ps1
@@ -1,5 +1,15 @@
+# Path to the VcXsrv executable
+$vcxsrvPath = "C:\Program Files\VcXsrv\xlaunch.exe"
+
+# Path to the VcXsrv configuration file
+$configFilePath = "C:\Users\eza14\Documents\config.xlaunch"
+
+# Start VcXsrv with the configuration file
+Start-Process -FilePath $configFilePath
+
 # Set the DISPLAY environment variable
 $env:DISPLAY="host.docker.internal:0.0"
 
-# Run Docker container with the specified command
-docker run -ti --rm -e DISPLAY="host.docker.internal:0.0" -v /tmp/.X11-unix:/tmp/.X11-unix -v "${PWD}:/home/developer/eclipse-workspace:rw" ezavvari/eclipse-container:latest /usr/local/bin/eclipse -data /home/developer/eclipse-workspace/eclipse-workspace
+# Run the Docker command
+docker pull ezavvari/eclipse-docker:latest
+docker run --name docker-container -ti --rm -e DISPLAY=$env:DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v "${PWD}:/home/developer/eclipse-workspace:rw" ezavvari/eclipse-docker:latest /usr/local/bin/eclipse -data /home/developer/eclipse-workspace/eclipse-workspace
