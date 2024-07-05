0e6f0f158ffede76616fea29bc5f010f30baa439
Elyar <elyar.zavary@gmail.com>
2024-07-05 15:12:12 -0700

    some necessary commits

--- windows/run_docker.ps1
+++ windows/run_docker.ps1
@@ -12,4 +12,4 @@ $env:DISPLAY="host.docker.internal:0.0"
 
 # Run the Docker command
 docker pull ezavvari/eclipse-docker:latest
-docker run --name docker-container -ti --rm -e DISPLAY=$env:DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v "${PWD}:/home/developer/eclipse-workspace:rw" ezavvari/eclipse-docker:latest /usr/local/bin/eclipse -data /home/developer/eclipse-workspace/eclipse-workspace
+docker run --name docker-container -ti --rm -e DISPLAY=$env:DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v "${PWD}"/eclipse-workspace/:/home/developer/eclipse-workspace:rw ezavvari/eclipse-docker:latest /usr/local/bin/eclipse -data /home/developer/eclipse-workspace/
