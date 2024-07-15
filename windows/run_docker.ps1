ab4b01d6bf44c1142a857530e5ccf7bfaf2f0610
Elyar <elyar.zavary@gmail.com>
2024-07-15 10:10:47 -0700

    powershell script to run the container in windows updated

--- windows/run_docker.ps1
+++ windows/run_docker.ps1
@@ -2,7 +2,7 @@
 $vcxsrvPath = "C:\Program Files\VcXsrv\xlaunch.exe"
 
 # Path to the VcXsrv configuration file
-$configFilePath = "C:\Users\eza14\Documents\config.xlaunch"
+$configFilePath = "C:\temp\config.xlaunch"
 
 # Start VcXsrv with the configuration file
 Start-Process -FilePath $configFilePath
@@ -10,6 +10,47 @@ Start-Process -FilePath $configFilePath
 # Set the DISPLAY environment variable
 $env:DISPLAY="host.docker.internal:0.0"
 
+# Define the folder path
+$folderPath = "U:\eclipse-workspace"
+
+# Define paths
+$userName = $env:USERNAME
+$sourcePath = "U:\eclipse-workspace"
+$destinationPath = "C:\Users\$userName\Desktop\eclipse-workspace"
+
+# Step 1: Create U:/eclipse-workspace and C:\Users\$userName\Desktop\eclipse-workspace if it doesn't exist
+if (-Not (Test-Path -Path "U:\eclipse-workspace")) {
+    New-Item -Path "U:\eclipse-workspace" -ItemType Directory
+    Write-Output "Created folder: $sourcePath"
+} else {
+    Write-Output "Folder already exists: U:\eclipse-workspace"
+}
+
+if (-Not (Test-Path -Path "C:\Users\$userName\Desktop\eclipse-workspace")) {
+    New-Item -Path "C:\Users\$userName\Desktop\eclipse-workspace" -ItemType Directory
+    Write-Output "Created folder: $sourcePath"
+} else {
+    Write-Output "Folder already exists: C:\Users\$userName\Desktop\eclipse-workspace"
+}
+
+# Step 2: Delete C:/Users/$username/Desktop/eclipse-workspace\*
+Remove-Item -Path "C:\Users\$userName\Desktop\eclipse-workspace\*" -Recurse -Force
+
+# Step 3: Copy contents from U:/eclipse-workspace to C:/Users/$username/Desktop/eclipse-workspace
+Copy-Item -Path "U:eclipse-workspace\*" -Destination "C:\Users\$userName\Desktop\eclipse-workspace\" -Recurse
+Write-Output "Copied contents from $sourcePath to $destinationPath"
+
 # Run the Docker command
 docker pull ezavvari/eclipse-docker:latest
-docker run --name docker-container -ti --rm -e DISPLAY=$env:DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v "${PWD}"/eclipse-workspace/:/home/developer/eclipse-workspace:rw ezavvari/eclipse-docker:latest /usr/local/bin/eclipse -data /home/developer/eclipse-workspace/
+docker run --name docker-container -ti --rm -e DISPLAY=$env:DISPLAY --user 1000:1000 -v /tmp/.X11-unix:/tmp/.X11-unix -v "${PWD}:/home/xubuntu/eclipse-workspace:rw" ezavvari/eclipse-docker:latest /usr/local/bin/eclipse -data /home/xubuntu/eclipse-workspace/eclipse-workspace
+
+# Step 5: Delete U:/eclipse-workspace
+Remove-Item -Path "U:\eclipse-workspace\*" -Recurse -Force
+Write-Output "Deleted folder: $sourcePath"
+
+# Step 6: Copy contents from C:/Users/$username/Desktop/eclipse-workspace back to U:/eclipse-workspace
+Copy-Item -Path "C:\Users\$userName\Desktop\eclipse-workspace\*" -Destination "U:\eclipse-workspace\" -Recurse
+Write-Output "Copied contents back from $destinationPath to $sourcePath"
+
+# Step 7: Delete Delete C:/Users/$username/Desktop/eclipse-workspace\
+Remove-Item -Path "C:\Users\$userName\Desktop\eclipse-workspace" -Recurse -Force
